68063212cc98d140a5df0bfb0358bf8ed7f79cc8
Lewis Clark <lewis.clark@me.com>
2022-04-28 09:38:53 +0100

    Base Files

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    content: ["./templates/**/*.{html,js,twig}"],
+    theme: {
+      extend: {
+        keyframes: {
+          'move-bg': {
+            to: {
+              backgroundPosition: '400% 0',
+            },
+          },
+        },
+        fontFamily: {
+          wotfardReg: ["Wotfard-Regular", "sans-serif"],
+          wotfardMed: ["Wotfard-Medium", "sans-serif"],
+          wotfardLight: ["Wotfard-Light", "sans-serif"],
+        },
+        animation: {
+          'move-bg': 'move-bg 8s infinite linear',
+        },
+      },
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
+  }
