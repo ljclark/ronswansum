0d58a20a445f399d80a52098d89b74ce8711ef07
Lewis Clark <lewis.clark@me.com>
2022-05-09 15:48:02 +0100

    Open graph images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,23 @@
 module.exports = {
-    content: ["./templates/**/*.{html,js,twig}"],
-    theme: {
-      extend: {
-        keyframes: {
-          'move-bg': {
-            to: {
-              backgroundPosition: '400% 0',
-            },
+  content: ["./templates/**/*.{html,js,twig}"],
+  theme: {
+    extend: {
+      keyframes: {
+        "move-bg": {
+          to: {
+            backgroundPosition: "400% 0",
           },
         },
-        fontFamily: {
-          wotfardReg: ["Wotfard-Regular", "sans-serif"],
-          wotfardMed: ["Wotfard-Medium", "sans-serif"],
-          wotfardLight: ["Wotfard-Light", "sans-serif"],
-        },
-        animation: {
-          'move-bg': 'move-bg 8s infinite linear',
-        },
+      },
+      fontFamily: {
+        wotfardReg: ["Wotfard-Regular", "sans-serif"],
+        wotfardMed: ["Wotfard-Medium", "sans-serif"],
+        wotfardLight: ["Wotfard-Light", "sans-serif"],
+      },
+      animation: {
+        "move-bg": "move-bg 8s infinite linear",
       },
     },
-    plugins: [
-      require('@tailwindcss/typography'),
-    ],
-  }
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
